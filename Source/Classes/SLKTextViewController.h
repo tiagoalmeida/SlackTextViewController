db4ce1b30cc54ae9cefbe6347ae9e262d81dd2bf
paul <paul@whisper.sh>
2015-02-09 22:47:04 -0800

    Reconfigured SLKTextViewController to accept a child controller, responsible for its own collection view

--- Source/Classes/SLKTextViewController.h
+++ Source/Classes/SLKTextViewController.h
@@ -46,6 +46,8 @@ typedef NS_ENUM(NSUInteger, SLKKeyboardStatus) {
 /** @name A drop-in UIViewController subclass with a growing text input view and other useful messaging features. */
 NS_CLASS_AVAILABLE_IOS(7_0) @interface SLKTextViewController : UIViewController <UITextViewDelegate, UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource, UIGestureRecognizerDelegate, UIAlertViewDelegate>
 
+@property (nonatomic, strong) UICollectionViewController *childCollectionController;
+
 /** The main table view managed by the controller object. Created by default initializing with -init or initWithNibName:bundle: */
 @property (nonatomic, readonly) UITableView *tableView;
 
@@ -111,6 +113,8 @@ NS_CLASS_AVAILABLE_IOS(7_0) @interface SLKTextViewController : UIViewController 
 ///------------------------------------------------
 /// @name Initialization
 ///------------------------------------------------
+
+- (instancetype)initWithChildCollectionController:(UICollectionViewController *)childCollectionController;
 
 /**
  Initializes a text view controller to manage a table view of a given style.
--- Source/Classes/SLKTextViewController.m
+++ Source/Classes/SLKTextViewController.m
@@ -88,6 +88,21 @@ NSString * const SLKKeyboardDidHideNotification =   @"SLKKeyboardDidHideNotifica
     return [self initWithTableViewStyle:UITableViewStylePlain];
 }
 
+- (instancetype)initWithChildCollectionController:(UICollectionViewController *)childCollectionController
+{
+    NSAssert([self class] != [SLKTextViewController class], @"Oops! You must subclass SLKTextViewController.");
+    
+    if (self = [super initWithNibName:nil bundle:nil])
+    {
+        _childCollectionController = childCollectionController;
+        
+        self.scrollViewProxy = childCollectionController.collectionView;
+        
+        [self _commonInit];
+    }
+    return self;
+}
+
 - (instancetype)initWithTableViewStyle:(UITableViewStyle)style
 {
     NSAssert([self class] != [SLKTextViewController class], @"Oops! You must subclass SLKTextViewController.");
@@ -167,8 +182,16 @@ NSString * const SLKKeyboardDidHideNotification =   @"SLKKeyboardDidHideNotifica
 - (void)loadView
 {
     [super loadView];
-        
-    [self.view addSubview:self.scrollViewProxy];
+    
+    if (_childCollectionController) {
+        [self addChildViewController:_childCollectionController];
+        [self.view addSubview:_childCollectionController.collectionView];
+        [_childCollectionController didMoveToParentViewController:self];
+    }
+    else {
+        [self.view addSubview:self.scrollViewProxy];
+    }
+    
     [self.view addSubview:self.autoCompletionView];
     [self.view addSubview:self.typingIndicatorView];
     [self.view addSubview:self.textInputbar];
@@ -251,6 +274,7 @@ NSString * const SLKKeyboardDidHideNotification =   @"SLKKeyboardDidHideNotifica
 
 - (UITableView *)tableViewWithStyle:(UITableViewStyle)style
 {
+    
     if (!_tableView)
     {
         _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:style];
